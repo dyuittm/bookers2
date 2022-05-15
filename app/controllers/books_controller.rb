afd4bb787775aeefb52958d4095acf77bbf894a1
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-05-15 09:13:52 +0000

    [Update]4-task

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,13 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+end
