d9d333754128512c38b700897c198d245551c604
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-05-16 02:18:17 +0000

    [Update]4-task

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :profile_image
   has_many :book, dependent: :destroy
+  has_one_attached :profile_image
+
+  def get_profile_image(width,height)
+    unless profile_imege.attached?
+      file_path = Rails.rooy.join('app/assets_images/no/image.jpeg')
+      profile_image.attach(io: File.open(file_path),filename: 'default-image.jpg',content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height].processed)
+  end
 end
